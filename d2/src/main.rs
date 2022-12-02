250fffff8227c14dc2c871481ec004ec71b6160e
CroxxN <saradgaihre19@gmail.com>
2022-12-02 21:05:54 +0545

    day 2 part 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -2,25 +2,30 @@ use std::fs::read_to_string;
 
 fn calc(value: (&str, &str)) -> i32 {
     match value {
-        ("A", "Y") => 8,
-        ("B", "Z") => 9,
-        ("C", "X") => 7,
-        ("A", "X") => 4,
-        ("B", "Y") => 5,
-        ("C", "Z") => 6,
-        (_, value) => {
-            if value == "X" {
-                1
-            } else if value == "Y" {
-                2
-            } else {
-                3
-            }
-        }
+        (value, "Z") => match value {
+            "A" => 8,
+            "B" => 9,
+            "C" => 7,
+            _ => 0,
+        },
+        (value, "Y") => match value {
+            "A" => 4,
+            "B" => 5,
+            "C" => 6,
+            _ => 0,
+        },
+
+        (value, "X") => match value {
+            "A" => 3,
+            "B" => 1,
+            "C" => 2,
+            _ => 0,
+        },
+        _ => 0,
     }
 }
 
-fn part_one() {
+fn solve() {
     let mut total = 0_i32;
     let things = read_to_string("./input.txt").unwrap();
     things
@@ -34,5 +39,5 @@ fn part_one() {
 }
 
 fn main() {
-    part_one();
+    solve();
 }
